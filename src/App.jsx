705232c4c35b84b7ba142bd56b75f14f87d7f3e3
Matheus Carvalho <carvalho.bfr@gmail.com>
2020-03-10 15:54:28 +0000

    lab done without bonus

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,33 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Route } from 'react-router-dom';
+import Home from './views/Home';
+import HomeLink from './views/HomeLink';
+import Beer from './views/Beer';
+import RandomBeer from './views/random-beer';
+import AllBeers from './views/AllBeers';
+import NewBeer from './views/new-beer';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        To get started, edit <code>src/App.jsx</code> and save to reload.
+        <div id="root">
+          <div>
+            <div className="row">
+              <HomeLink />
+              <Route exact path="/new-beer" component={NewBeer} />
+              <Route exact path="/" component={Home} />
+              <Route exact path="/random-beer" component={RandomBeer} />
+              <Route path="/beers/:beerId" component={Beer} />
+              <Route exact path="/beers" component={AllBeers} />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
 }
 
 export default App;
+
---
+++ src/views/Home.jsx
@@ -0,0 +1,61 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+class Home extends Component {
+  render() {
+
+    return (
+      <div className="container">
+        <Link to="/beers">
+          <div>
+            <div>
+              <img
+                style={{ width: "50vw" }}
+                src="../images/beers.png"
+                alt=""
+              />
+            </div>
+            <h2>All Beers</h2>
+            <p>
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            </p>
+          </div>
+        </Link>
+
+        <Link to="/random-beer">
+          <div>
+            <div>
+              <img
+                style={{ width: "50vw" }}
+                src="../images/random-beer.png"
+                alt=""
+              />
+            </div>
+            <h2>Random Beer</h2>
+            <p>
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            </p>
+          </div>
+        </Link>
+
+        <Link to="/new-beer">
+          <div>
+            <div>
+              <img
+                style={{ width: "50vw" }}
+                src="../images/new-beer.png"
+                alt=""
+              />
+            </div>
+            <h2>New Beer</h2>
+            <p>
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            </p>
+          </div>
+        </Link>
+      </div>
+    );
+  }
+}
+
+export default Home;
